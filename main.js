24707009d48ef2fcf52dea0c56f06df7b9c1f31e
vickykr78 <vickykr78@users.noreply.github.com>
2021-09-30 22:55:11 +0530

    login

---
+++ main.js
@@ -0,0 +1,9 @@
+function change(){
+    var e=document.querySelector('.container');
+    e.classList.toggle('active');
+   
+
+    var e=document.querySelector('.container1');
+    e.classList.toggle('active');
+    
+}
